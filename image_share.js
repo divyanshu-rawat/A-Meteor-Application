a33ada2a70f20cceecdf04bfaa680cedc9fac198
divyanshu <divyanshu.r46956@gmail.com>
2017-08-07 23:24:41 +0530

    Added new features in Gallery #1 User can only add Images if they are loggedIn #2 Images can be filtered via username !

--- image_share.js
+++ image_share.js
@@ -2,19 +2,102 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
-   Template.images.helpers({images:
-    Images.find({}, {sort:{createdOn: -1, rating:-1}})
-  });
+
+     Template.images.helpers({
+
+      images:function () {
+
+        if(Session.get('filterSession')){
+          console.log('Session Set !');
+          return Images.find({createdBy:Session.get('filterSession')});
+        }
+        else{
+            return Images.find({}, {sort:{createdOn: -1, rating:-1}})
+        }
+      },
+
+      getUser:function(user_id){
+        var user = Meteor.users.findOne({_id:user_id});
+        if (user){
+          return user.username;
+        }
+        else {
+          return "anon";
+        }
+      },
+
+      checkFilter:function () {
+        if(Session.get('filterSession')){
+          return true
+        }
+        else{
+          return false;
+        }
+
+      },
+
+      getFilterUser:function () {
+
+        if(Session.get('filterSession')){
+            var user = Meteor.users.findOne({_id:Session.get('filterSession')});
+            return user.username;
+        }
+        else{
+          return false;
+        }
+      }
+              
+    });
+
+     Template.body.helpers({
+
+        username:function () {
+          if(Meteor.user()){
+            // console.log(Meteor.user())
+            return Meteor.user().username;
+          }
+          else{
+            return 'Anonymous User !'
+          }
+        }
+
+
+     });
+
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_AND_EMAIL"
+    });
 
    Template.images.events({
 
-    // 'click .js-image':function(event){
-    //     $(event.target).css("width", "50px");
+    'click .js-set-image-filter':function(event){
+        // console.log(this);
+
+        // console.log(Images.find({createdBy:this.createdBy}));
+
+        Session.set('filterSession', this.createdBy);
+
+    },
+
+      'click .js-unset-image-filter':function(event){
+        // console.log(this);
+
+        // console.log(Images.find({createdBy:this.createdBy}));
+
+        Session.set('filterSession', undefined);
+
+    },
+
+ 
+    // 'click .js-display-user':function (event) {
+    //   console.log(Meteor.user()._id);
+    //   console.log(Meteor.users.findOne({"_id":Meteor.user()._id}).emails[0].address);
     // },
 
     'click .js-del-image':function(event){
 
-    console.log('Event Fired !');
+       console.log('Event Fired !');
        var image_id = this._id;
        console.log(image_id);
        // use jquery to hide the image component
@@ -27,7 +110,7 @@ if (Meteor.isClient) {
     'click .js-rate-image':function(event){
       var rating = $(event.currentTarget).data("userrating");
       console.log(rating);
-      var image_id = this.id;
+      var image_id = this.data_id;
       console.log(image_id);
 
       Images.update({_id:image_id}, 
@@ -49,7 +132,9 @@ if (Meteor.isClient) {
       Images.insert({
         img_src:img_src, 
         img_alt:img_alt, 
-        createdOn:new Date()
+        createdOn:new Date(),
+        createdBy:Meteor.user()._id
+
       });
        $("#image_add_form").modal('show');
       return false;
