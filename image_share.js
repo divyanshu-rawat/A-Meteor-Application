9e6f8fb746d39f8c90c5bfe58ae083a620799241
divyanshu <divyanshu.r46956@gmail.com>
2017-08-08 00:31:32 +0530

    infinite scroll Implemented !

--- image_share.js
+++ image_share.js
@@ -3,6 +3,25 @@ Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
 
+    Session.set("imageLimit", 8);
+
+      lastScrollTop = 0; 
+      $(window).scroll(function(event){
+        // test if we are near the bottom of the window
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+          // where are we in the page? 
+          var scrollTop = $(this).scrollTop();
+          // test if we are going down
+          if (scrollTop > lastScrollTop){
+            // yes we are heading down...
+           Session.set("imageLimit", Session.get("imageLimit") + 4);
+          }
+
+          lastScrollTop = scrollTop;
+        }
+            
+      })
+
      Template.images.helpers({
 
       images:function () {
@@ -12,7 +31,7 @@ if (Meteor.isClient) {
           return Images.find({createdBy:Session.get('filterSession')});
         }
         else{
-            return Images.find({}, {sort:{createdOn: -1, rating:-1}})
+            return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});
         }
       },
 
@@ -57,7 +76,7 @@ if (Meteor.isClient) {
             return Meteor.user().username;
           }
           else{
-            return 'Anonymous User !'
+            return 'Anonymous'
           }
         }
 
